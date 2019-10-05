01022206bc7e7e5cd21ab579a3fd02d605c0e457
AnshKhurana <anshkhurana1507@gmail.com>
2019-10-05 12:52:58 +0530

    Basic template scripts

---
+++ Assets/Scripts/Player_class.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_class : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public const int StunDuration = 4;
+    public float CPI;
+    public float StunTimer;
+    public int state; // 0 - free movement, 1 - stunned, 2 - studying
+
+
+    void Start()
+    {
+        CPI = 8.0f;
+        StunTimer = 0;
+        state = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
