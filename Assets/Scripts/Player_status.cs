91c2d0b55be9d27db9009243d2544a25fa86085a
AnshKhurana <anshkhurana1507@gmail.com>
2019-10-05 18:47:26 +0530

    Added locking Table

---
+++ Assets/Scripts/Player_status.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_status : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float CPI;
+    public bool isHit;
+    public bool lockControls;
+    public bool stunLock;
+    public int stunTimer = 4;
+
+    void Start()
+    {
+        CPI = 6.0f;
+        isHit = false;
+        lockControls = false;
+        stunLock = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
