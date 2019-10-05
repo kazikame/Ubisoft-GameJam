9e84e99d376e446ff9b6f24f9852f7bf849a7d52
Akshat Chugh <akshatchugh3@gmail.com>
2019-10-05 16:32:06 +0530

    Board manager stuff

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -34,27 +34,27 @@ public class BoardManager : MonoBehaviour
     {
         Vector3 scale = floor.transform.localScale;
         Vector3 pos = floor.transform.position;
-        float xrangel = pos.x - 0.5f*scale.x + 0.5f;
-        float xranger = pos.x + 0.5f*scale.x - 0.5f;
-        float zrangel = pos.z - 0.5f*scale.z + 0.5f;
-        float zranger = pos.z + 0.5f*scale.z - 0.5f;
+        float xrangel = pos.x - 0.5f*scale.x - 0.5f;
+        float xranger = pos.x + 0.5f*scale.x + 0.5f;
+        float zrangel = pos.z - 0.5f*scale.z - 0.5f;
+        float zranger = pos.z + 0.5f*scale.z + 0.5f;
         for (float x = xrangel; x < xranger; x++)
         {
        
-            Instantiate(outerwall, new Vector3(x, 0f, zrangel), Quaternion.identity);
+            Instantiate(outerwall, new Vector3(x, 0.5f*(-scale.y+outerwall.transform.localScale.y), zrangel), Quaternion.identity);
 
         }
         for(float z = zrangel; z < zranger; z++)
         {
-            Instantiate(outerwall, new Vector3(xranger, 0f, z), Quaternion.identity);
+            Instantiate(outerwall, new Vector3(xranger, 0.5f * (-scale.y + outerwall.transform.localScale.y), z), Quaternion.identity);
         }
         for(float x = xranger; x > xrangel; x--)
         {
-            Instantiate(outerwall, new Vector3(x, 0f, zranger), Quaternion.identity);
+            Instantiate(outerwall, new Vector3(x, 0.5f * (-scale.y + outerwall.transform.localScale.y), zranger), Quaternion.identity);
         }
         for(float z = zranger; z > zrangel; z--)
         {
-            Instantiate(outerwall, new Vector3(xrangel, 0f, z), Quaternion.identity);
+            Instantiate(outerwall, new Vector3(xrangel, 0.5f * (-scale.y + outerwall.transform.localScale.y), z), Quaternion.identity);
         }
 
         
@@ -64,23 +64,27 @@ public class BoardManager : MonoBehaviour
     
     void GiveRandomPosition(GameObject gameobject)
     {
-        Vector3 randomposition;
+        Vector3 randomposition = new Vector3();
         do
         {
+           
             int randindex = Random.Range(0, positions.Count);
             randomposition = positions[randindex];
+            
         } while ((Physics.OverlapBox(randomposition, gameobject.transform.localScale / 2)).Length != 0);
         Instantiate(gameobject, randomposition, Quaternion.identity);
     }
     // Start is called before the first frame update
     void Awake()
     {
+        InitializePositions();
+        GiveRandomPosition(disk);
+        GiveRandomPosition(magazine);
         Instantiate(floor, new Vector3(0f, 0f, 0f), Quaternion.identity);
         Instantiate(table, new Vector3(0f, 1f, 0f), Quaternion.identity);
         Instantiate(player, new Vector3(0f, 0.5f, floor.transform.position.z + 0.4f * floor.transform.localScale.z), Quaternion.Euler(new Vector3(0,180,0)));
         PlaceWalls();
-        GiveRandomPosition(disk);
-        GiveRandomPosition(magazine);
+        
 
     }
 
