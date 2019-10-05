cca9b7c54ecbeabbec502701e201b3987ce6dd60
AnshKhurana <anshkhurana1507@gmail.com>
2019-10-05 15:12:01 +0530

    Adds through object

--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Table : MonoBehaviour
 {
+
+    private bool studying;
+    public GameObject[] Players;
     // Start is called before the first frame update
     void Start()
     {
-        
+        studying = false;
     }
 
     // Update is called once per frame
