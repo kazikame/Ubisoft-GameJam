7130769758cc60f1e0cb571caa7f35420a124bea
Saksham Goel <38839591+kazikame@users.noreply.github.com>
2019-10-05 16:29:31 +0530

    throw completed

--- Assets/Throw.cs
+++ Assets/Throw.cs
@@ -7,13 +7,39 @@ public class Throw : MonoBehaviour {
     public GameObject sphere;
     public int throwSpeed = 100;
     GameObject throwObject = null;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "pickable")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //If object already exists, reset it and leave it in the world
+                if (throwObject != null)
+                {
+                    throwObject.transform.parent = null;
+                    throwObject.GetComponent<Collider>().enabled = true;
+                    throwObject.transform.position -= new Vector3(0, 1, 0);
+                    throwObject.tag = "pickable";
+                    throwObject = null;
+                }
+                //TODO: Smoothen this!
+                GameObject obj = other.gameObject;
+                obj.transform.parent = this.gameObject.transform;
+                obj.transform.position += (new Vector3(0, 1, 0)) + Quaternion.Euler(0, 90, 0) * transform.forward;
+                throwObject = obj;
+                throwObject.tag = "picked";
+                throwObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+    }   
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && throwObject == null)
-        {
-            throwObject = Instantiate(sphere, transform) as GameObject;
-            throwObject.transform.position += (new Vector3(0, 1, 0)) + Quaternion.Euler(0, 90, 0) * transform.forward;
-        }
+        //if (Input.GetKeyDown(KeyCode.Space) && throwObject == null)
+        //{
+        //    throwObject = Instantiate(sphere, transform) as GameObject;
+        //    throwObject.transform.position += (new Vector3(0, 1, 0)) + Quaternion.Euler(0, 90, 0) * transform.forward;
+        //}
         if (Input.GetKeyDown(KeyCode.X))
         {
             if (throwObject != null)
