90d4b2952466c311b30d8bbc419ed00c5d34818a
AnshKhurana <anshkhurana1507@gmail.com>
2019-10-05 16:30:56 +0530

    New table

--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class Table : MonoBehaviour
 {
 
-    private bool studying;
+    public bool studying;
     public GameObject[] Players;
+    private float updatePositionTime = 5f;
+    private float elapsed;
     // Start is called before the first frame update
     void Start()
     {
         studying = false;
     }
 
+    //bool isNearKeyPress(GameObject player)
+    //{
+    //    if (Vector3.Distance(player.transform.position, )
+    //    if(Input.GetKey(player.GetComponent<knight_walk>.Study))
+    //}
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Something near");
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject nearPlayer = other.gameObject;
+            if (Input.GetKey(nearPlayer.GetComponent<knight_walk>().Study))
+            {
+                studying = true;
+            }
+
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        //for (int i = 0; i < Players.Length; i++)
+        //{
+        //    if (isNearAndKeyPress(Players[i]))
+        //    {
+        //        studying = true;
+
+        //    }
+        //}
+        if (studying)
+        {
+
+        }
     }
 }
